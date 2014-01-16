acd1a22fc2463b6341a227419276e4365018be67
Nicolas Le Gall <neovov@gmail.com>
2014-01-16 16:11:28 +1000

    Removing TabsBlock and TabBlock since it was too much a pain to make it actually work

--- _plugins/Fabuloos.rb
+++ _plugins/Fabuloos.rb
@@ -57,45 +57,9 @@ module Fabuloos
 				'<aside class="lead">' + content + "</aside>"
 			end
 		end
-
-		class TabsBlock < Liquid::Block
-			def render(context)
-				'<div class="tab-content">' + super + "</div>"
-			end
-		end
-
-		class TabBlock < Liquid::Block
-			def initialize(tag_name, tab, tokens)
-				super
-				@tab = tab.strip
-			end
-
-			def render(context)
-				return "" if @tab.empty?
-
-				site      = context.registers[:site]
-				converter = site.getConverterImpl(Jekyll::Converters::Markdown)
-
-				lines = super.rstrip.split(/\r\n|\r|\n/).select { |line| line.size > 0 }
-				indentation = lines.map do |line|
-					match = line.match(/^(\s+)[^\s]+/)
-					match ? match[1].size : 0
-				end
-				indentation = indentation.min
-
-				content = indentation ? super.gsub(/^#{' |\t' * indentation}/, '') : super
-				content = converter.convert(content)
-				content = content.strip # Strip again otherwise jekyll will go mad
-
-				'<div id="' + @tab + '" class="tab-pane' + (@tab == "browsers" ? " active" : "") + '">' + content + "</div>"
-			end
-		end
 	end
 end
 
 Liquid::Template.register_tag("alert", Fabuloos::Tags::AlertBlock)
 Liquid::Template.register_tag("aside", Fabuloos::Tags::AsideBlock)
-Liquid::Template.register_tag("tabs",  Fabuloos::Tags::TabsBlock)
-Liquid::Template.register_tag("tab",   Fabuloos::Tags::TabBlock)
-
 Liquid::Template.register_filter(Fabuloos::Filters)
